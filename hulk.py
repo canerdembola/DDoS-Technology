5405d6e70adefc7e9343e1615639e90b7afd8609
canerdembola <34920271+canerdembola@users.noreply.github.com>
2018-01-04 22:40:04 +0300

    Update hulk.py

--- hulk.py
+++ hulk.py
@@ -146,9 +146,9 @@ else:
 		url = sys.argv[1]
 		if url.count("/")==2:
 			url = url + "/"
-		m = re.search('http?s\://([^/]*)/?.*', url)
+		m = re.search('https\http\://([^/]*)/?.*', url)
 		host = m.group(1)
-		for i in range(500):
+		for i in range(99999999999999999999999999):
 			t = HTTPThread()
 			t.start()
 		t = MonitorThread()
